eac804379f5c383499e42ecbe4a7ca4212488aae
Usman Hussnain <21ntucs1284@student.ntu.edu.pk>
2024-07-01 11:12:44 +0500

    Email_verification

--- authentication/urls.py
+++ authentication/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-    path('accounts/', include('allauth.urls')),
-    
+    path('', include('base.urls'))
 ]
